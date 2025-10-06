3b45cf227d3ecd85d5e99f6ce6588e6222c41d39
Faiz U. Shaikh <61307003+codealive-fs@users.noreply.github.com>
2025-10-06 12:11:23 +0500

    Delete scripts/stop_server.sh

--- scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-echo "Stopping node-app (if running)..."
-
-if systemctl list-units --full -all | grep -Fq "node-app.service"; then
-  if systemctl is-active --quiet node-app; then
-    systemctl stop node-app || true
-  fi
-fi
