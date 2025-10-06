58adf72315f573d0f2835176bf3ea1a1a3a8060e
Faiz U. Shaikh <61307003+codealive-fs@users.noreply.github.com>
2025-10-06 12:04:38 +0500

    Update validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,12 +1,12 @@
 
-#!/bin/bash
-set -euo pipefail
-echo "Validating node-app is running..."
+# #!/bin/bash
+# set -euo pipefail
+# echo "Validating node-app is running..."
 
-sleep 5
-STATUS=$(systemctl is-active node-app || true)
+# sleep 5
+# STATUS=$(systemctl is-active node-app || true)
 
-if [ "$STATUS" != "active" ]; then
-  echo "node-app service is not active!"
-  exit 1
-fi
+# if [ "$STATUS" != "active" ]; then
+#   echo "node-app service is not active!"
+#   exit 1
+# fi
