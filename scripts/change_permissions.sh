c34eac386252138c35968175113a51bc087d19ee
Faiz U. Shaikh <61307003+codealive-fs@users.noreply.github.com>
2025-10-06 12:06:44 +0500

    Update change_permissions.sh

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-set -euo pipefail
-echo "Fixing ownership and permissions..."
+# #!/bin/bash
+# set -euo pipefail
+# echo "Fixing ownership and permissions..."
 
-chown -R www-data:www-data /var/www/node-app
-find /var/www/node-app -type f -exec chmod 644 {} \;
-find /var/www/node-app -type d -exec chmod 755 {} \;
+# chown -R www-data:www-data /var/www/node-app
+# find /var/www/node-app -type f -exec chmod 644 {} \;
+# find /var/www/node-app -type d -exec chmod 755 {} \;
 
-# make sure scripts are executable
-chmod +x /var/www/node-app/scripts/*.sh || true
+# # make sure scripts are executable
+# chmod +x /var/www/node-app/scripts/*.sh || true
 
